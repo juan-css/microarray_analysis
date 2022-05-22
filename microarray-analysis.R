04fdddfa3d7de954b9ec513f592e882a931f0081
juan-css <juancss99@gmail.com>
2022-05-22 17:29:32 -0300

    DE Analysis and volcano plot

--- microarray-analysis.R
+++ microarray-analysis.R
@@ -16,10 +16,8 @@
 
 ########## 1. Get data  --------------
 
-# Baixando dados do estudo GSE54992
+# Data analysis
 # BiocManager::install("GEOquery")
-# BiocManager::install("affy")
-# BiocManager::install("arrayQualityMetrics")
 library(GEOquery)
 library(affy)
 library(arrayQualityMetrics)
@@ -28,35 +26,48 @@ library(tidyr)
 library(readr)
 library(biomaRt)
 library(data.table)
+library(limma)
 options(stringsAsFactors = FALSE)
+
+# Plots
+library(ggplot2)
+library(ggrepel)
 
 # Set directory
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 dir.create("data")
 setwd("./data")
 
+# Baixando dados do estudo GSE54992
 gse_ID <- "GSE54992"
 gpl_ID <- "GPL570"
 path_gse = paste0('data/', gse_ID, '/')
 
 #baixando os dados do GEO
 gse <- getGEO(gse_ID)
-show(gse_ID)
 
 # baixando dados brutos do estudo (.CEL)
 getGEOSuppFiles(gse_ID)
 
-# alvando os dados de expressão em um arquivo
+# Salvando os dados de expressão em um arquivo
 table_expression <- exprs(gse[[1]])
-write.table(table_expression, "GSE54992/table_expression_array.tsv", sep = "\t", col.names = TRUE, row.names = FALSE)
+write.table(table_expression, 
+            "GSE54992/table_expression_array.tsv", 
+            sep = "\t", 
+            col.names = TRUE, 
+            row.names = FALSE)
 
 # visualizar o phenodata
-table_phenodata <- pData(phenoData(gse[[1]]))
-write.table(table_phenodata, "GSE54992/table_phenodata.tsv", sep = "\t")
+metadata <- pData(phenoData(gse[[1]]))
+write.table(metadata, 
+            "GSE54992/metadata.tsv", 
+            sep = "\t")
 
 # obtendo dados da plataforma
 probe_table <- gse[[1]]@featureData@data
-write.table(probe_table, "GSE54992/annotation_platform.tsv", sep = "\t")
+write.table(probe_table, 
+            "GSE54992/annotation_platform.tsv",
+            sep = "\t")
 
 
 
@@ -83,12 +94,17 @@ cel_files <- list.files(path = paste0(path_gse,'GSE54992_RAW'),
 rawdata <- ReadAffy(filenames = cel_files)
 
 # Create a directory to save AQM pre normalization (non-normalized)
-dir_aqm_non_norm <- paste0("intermediate/aqm_teste/", gse_ID, "_", gpl_ID, "_AQM_non_norm")
+dir_aqm_non_norm <- paste0("intermediate/aqm_teste/", 
+                           gse_ID, 
+                           "_", 
+                           gpl_ID, 
+                           "_AQM_non_norm")
 
 # Running AQM
 arrayQualityMetrics(expressionset = rawdata, 
                     outdir = dir_aqm_non_norm, 
-                    force = TRUE, do.logtransform = TRUE)
+                    force = TRUE, 
+                    do.logtransform = TRUE)
 
 raw_expr <- rawdata@assayData[["exprs"]]
 
@@ -107,7 +123,11 @@ expr_norm <- rma(rawdata)
 
 
 #### ----- c. Run AQM pos normalization -----####
-dir_aqm_norm <- paste0("intermediate/aqm_teste/", gse_ID, "_", gpl_ID, "_AQM_norm")
+dir_aqm_norm <- paste0("intermediate/aqm_teste/",
+                       gse_ID, 
+                       "_",
+                       gpl_ID, 
+                       "_AQM_norm")
 
 # The expressionset needs to be the file that comes out ReadAffy (it's not just a simple dataframe, it's multiple lists together)
 arrayQualityMetrics(expressionset = expr_norm, 
@@ -116,7 +136,11 @@ arrayQualityMetrics(expressionset = expr_norm,
 
 # Save the normalized expression file, if case:
 expr_norm_final <- expr_norm@assayData[["exprs"]]
-write.table(expr_norm_final, "intermediate/expr_final_norm.csv", sep = "\t", row.names = TRUE, col.names = TRUE)
+write.table(expr_norm_final,
+            "intermediate/expr_final_norm.csv",
+            sep = "\t",
+            row.names = TRUE,
+            col.names = TRUE)
 
 ############ Attention! ############
 # Only remove outliers if the arrayQualityMetrics detects outlier in two or more tests.
@@ -126,7 +150,11 @@ write.table(expr_norm_final, "intermediate/expr_final_norm.csv", sep = "\t", row
 
 
 # ----- d. Remove outliers, renorm and control quality ----- ####
-dir_aqm_renorm <- paste0("intermediate/aqm_teste/", gse_ID, "_", gpl_ID, "_AQM_renorm")
+dir_aqm_renorm <- paste0("intermediate/aqm_teste/", 
+                         gse_ID, 
+                         "_",
+                         gpl_ID, 
+                         "_AQM_renorm")
 
 # Creating a vector with the outlier samples (see the table in the index)
 outlier_samples <- c("GSE54992_RAW/XXXXXX_OUTLIER_SAMPLE_NAME.CEL.gz", 
@@ -149,10 +177,14 @@ arrayQualityMetrics(expressionset = expr_renorm,
 
 # Save the renormalized expression data  
 expr_renorm_final <- expr_renorm@assayData[["exprs"]]
-write.table(expr_renorm_final, "intermediate/expr_final_norm_noOutlier.csv", sep = "\t", row.names = TRUE, col.names = TRUE)
+write.table(expr_renorm_final, 
+            "intermediate/expr_final_norm_noOutlier.csv",
+            sep = "\t",
+            row.names = TRUE, 
+            col.names = TRUE)
 
 # Clean useless data
-rm()
+rm(list=ls())
 
 
 
@@ -199,7 +231,7 @@ expr <- collapse.rows(expr = expr,
                       gene.col = 'geneName_gse', 
                       method = 'maxMean')
 
-expr = expr[expr$geneName_gse != "", ] 
+expr = expr[expr$geneName_gse != "", ]
 length(unique(expr$geneName_gse))
 expr = expr[,-2]
 
@@ -209,5 +241,156 @@ write.table(expr,
             row.names = FALSE, 
             col.names = TRUE)
 
+# Clean environment
+rm(list=ls())
+
+
+
+#### ----- 5. DEGs   --------------
+
+# ---- a. Set-up expression and metadata ----
+
+# Filter the samples to the samples you want
+metadata = read.delim("data/GSE54992/metadata.tsv")
+
+# Select the class you want to compare
+table(metadata$disease.state.ch1)
+classes_to_deg = c("healthy donor", "tuberculosis")
+metadata = metadata[metadata$disease.state.ch1 %in% classes_to_deg,]
+
+# Set expression table names
+expr = read.delim("intermediate/expr_table_collapsed.tsv", row.names = 1)
+colnames(expr) = gsub("\\_.*","", colnames(expr))
+
+# Filter expression table with the metadata
+# VERY IMPORTANT!! ALWAYS DO THIS!!
+print(paste("Total number of samples:",
+            nrow(metadata[metadata$geo_accession %in% colnames(expr),])))
+
+expr = expr[,colnames(expr) %in% metadata$geo_accession]
+
+# reorder expression matrix according to the order of the samplesinfo object
+# VERY IMPORTANT!! ALWAYS DO THIS!!
+expr <- expr[, metadata$geo_accession]
+
+# Check if all are in the same order
+all(metadata$Sample %in% colnames(expr))
+
+# Set the classes you need to compare
+metadata$Class = metadata$source_name_ch1
+
+# Remove useless information
+metadata$Class = gsub("PBMC from ", "", metadata$Class)
+
+# Classes
+table(metadata$Class)
+
+# ---- b. Limma ----
+
+# Design matrix experiment
+samples = metadata$Class
+samples = factor(samples)
+samples
+
+design.mat = model.matrix(~0+samples)
+colnames(design.mat) = levels(samples)
+design.mat
+
+# Contrast Matrix
+contrast.mat = makeContrasts(
+  test1 = TB - HC, # deg for treatment1
+  # test2 = TB - LTB, # In case of compare a third class
+  levels = design.mat
+)
+
+contrast.mat
+
+# Fit Limmma
+# Fit linear model to estimate T, N for each gene
+fit = lmFit(expr, design.mat)
+
+# Fit linear model to estimate a set of contrast, e.g. T-N 
+fit = contrasts.fit(fit, contrast.mat)
+
+# Given a microarray linear model fit, compute moderate t-statistics, 
+# moderate F-statistic and log-odds of differential expression by empirical Bayes 
+# moderation of the standard errors towards a common value.
+fit = eBayes(fit)
+
+
+# Comparing classes
+degs.test1 = topTable(fit, coef = "test1", 
+                      number = nrow(expr),
+                      adjust.method = 'fdr', 
+                      p.value=0.05, 
+                      lfc=log2(1))
+# deg.test2 = topTable(fit, coef = "test2", number = nrow(expr),
+#                      adjust.method = 'fdr', p.value=0.05, lfc=log2(1))
+
+
+dim(degs.test1)[1]
+dir.create('results/degs')
+write.table(degs.test1, 
+            'results/degs/degs_TB_vs_HC.tsv',
+            sep = '\t', 
+            row.names = T)
+
+
+
+
+#### ----- 5. PLOTs   --------------
+
+# ---- a. Volcano Plot ----
+
+#### Define DEGs
+degs.test1['deg_status'] <- 'no'
+degs.test1$deg_status[degs.test1$logFC > 1 & degs.test1$adj.P.Val < 0.05] <- "UP"
+degs.test1$deg_status[degs.test1$logFC < -1 & degs.test1$adj.P.Val < 0.05] <- "DOWN"
+
+# Set colors
+mycolors <- c("royalblue3", "red2", "lightgrey")
+names(mycolors) <- c("DOWN", "UP", "no")
+
+# Theme
+cleanup = 
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.line = element_line(color = "black"),
+    # legend.key = element_rect(fill = "white"),
+    axis.title =
+      ggplot2::element_text(size = 12),
+    axis.text = 
+      ggplot2::element_text(size = 12),
+    legend.text =
+      ggplot2::element_text(size = 12)
+  )
+
+# Set the labels
+degs.test1$label <- row.names(degs.test1)
+degs.test1$label[degs.test1$deg_status == "no"] <- NA
+
+# Plot
+pdf("results/degs/degs_volcano.pdf")
+ggplot(degs.test1, aes(
+  x = logFC,
+  y = -log10(adj.P.Val),
+  col = deg_status,
+  label = label
+)) +
+  geom_point() +
+  geom_text_repel(
+    max.overlaps = 30,
+    box.padding = 1,
+    segment.color = "lightgrey"
+  ) +
+  labs(x = "log2(Fold-Change)", y = "-log10(P.adjusted)") +
+  # geom_vline(xintercept=c(-1, 1), col="grey", linetype="dashed") +
+  # geom_hline(yintercept=-log10(0.05), col="grey", linetype="dashed") +
+  scale_colour_manual(values = mycolors) +
+  theme_minimal() +
+  cleanup
+dev.off()
 
 
